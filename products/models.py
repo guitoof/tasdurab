15db3b120314e84d2550dcf9c82d4508c7cc3b42
Guillaume Diallo-Mulliez <guillaume.diallo@ensta-paristech.fr>
2015-02-01 03:03:29 +0100

    Adding __unicode__ and __str__ methods to the models

--- products/models.py
+++ products/models.py
@@ -7,6 +7,12 @@ class Category(models.Model):
 
     title = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
+
 
 class Product(models.Model):
 
@@ -17,3 +23,9 @@ class Product(models.Model):
     owner = models.ForeignKey(UserProfile)
 
     pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
