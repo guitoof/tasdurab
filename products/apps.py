34cd51b23da56c06e83010593635f010e286b5f2
boisgard <anastasia.boisgard@gmail.com>
2015-02-05 00:02:29 +0100

    translating apps and models for admin panel

---
+++ products/apps.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*
+
+from django.apps import AppConfig
+
+class ProductsConfig(AppConfig):
+    name = "products"
+    verbose_name = u'Produit'
--- products/models.py
+++ products/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*
+
 from django.db import models
 from users.models import User
 
@@ -5,7 +7,11 @@ from users.models import User
 
 class Category(models.Model):
 
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Nom')
+
+    class Meta:
+        verbose_name = u'Catégorie'
+        verbose_name_plural = u'Catégories'
 
     def __unicode__(self):
         return self.title
@@ -17,12 +23,16 @@ class Category(models.Model):
 class Product(models.Model):
 
     category = models.ForeignKey(Category)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Nom')
     description = models.CharField(max_length=2048)
     expiry_date = models.DateTimeField('expiry date')
     owner = models.ForeignKey(User)
 
     pub_date = models.DateTimeField('date published')
+
+    class Meta:
+        verbose_name = u'Produit'
+        verbose_name_plural = u'Produits'
 
     def __unicode__(self):
         return self.title
