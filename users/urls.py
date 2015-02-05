ace1b28488d3aee2a514291490163242af8524a3
Guillaume Diallo-Mulliez <guillaume.diallo@ensta-paristech.fr>
2015-02-05 15:13:57 +0100

    Trying to integrate html attributes within view widget

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from users.views import UserDetailView, UserCreateView
 
 urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/$', UserDetailView.as_view(), name='detail'),
-    url(r'register$', UserCreateView.as_view(), name='create'),
+    url(r'register$', UserCreateView.as_view(), name='register'),
 )
--- users/views.py
+++ users/views.py
@@ -1,6 +1,20 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from users.models import User
+from users.forms import UserRegistrationForm
+
+
+class UserCreateView(CreateView):
+
+    model = User
+    fields = '__all__'
+    form_class = UserRegistrationForm
+
+    def get_context_data(self, **kwargs):
+        context = super(CreateView, self).get_context_data(**kwargs)
+        context['user'] = self.model
+        return context
+
 
 class UserDetailView(DetailView):
 
@@ -8,16 +22,5 @@ class UserDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(UserDetailView, self).get_context_data(**kwargs)
-        context['user'] = self.model
-        return context
-
-
-class UserCreateView(CreateView):
-
-    model = User
-    fields = '__all__'
-
-    def get_context_data(self, **kwargs):
-        context = super(UserCreateView, self).get_context_data(**kwargs)
         context['user'] = self.model
         return context
