9b03bae34a6388649b7f0cd45ba84cac738a40f2
Guillaume Diallo-Mulliez <guillaume.diallo@ensta-paristech.fr>
2015-02-24 14:56:11 +0100

    Closes #11

--- products/views.py
+++ products/views.py
@@ -3,11 +3,24 @@ from django.views.generic.edit import CreateView
 from products.models import Product, Category
 from products.forms import ProductForm
 
+from django.db.models import Q
+
 class ProductListView(ListView):
 
     def get_queryset(self):
-        """Return all the products"""
+        if "category" in self.request.GET and self.request.GET.get('category').isdigit():
+            return self.filter_by_category(self.request.GET.get('category'))
+        if "keyword" in self.request.GET:
+            return self.filter_by_keyword(self.request.GET.get('keyword'))
         return Product.objects.all()
+
+    def filter_by_keyword(self, keyword):
+        category_title_matches = Category.objects.filter(title__icontains=keyword)
+        return Product.objects.filter(Q(title__icontains=keyword) | Q(description__icontains=keyword) | Q(category=category_title_matches)).distinct().all()
+
+    def filter_by_category(self, category_id):
+        category = Category.objects.filter(pk=int(category_id))
+        return Product.objects.filter(category=category).all()
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
