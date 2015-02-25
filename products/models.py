2f7dc3d316bf45d55d09a94bcc170478ac604fcd
boisgard <anastasia.boisgard@gmail.com>
2015-02-25 22:56:24 +0100

    changing from DateTimeField to DateField

--- products/models.py
+++ products/models.py
@@ -23,9 +23,12 @@ class Product(models.Model):
 
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=255, verbose_name='Nom')
-    description = models.CharField(max_length=2048)
-    expiry_date = models.DateTimeField('expiry date')
+    description = models.CharField(max_length=2048, verbose_name= 'Description')
+    expiry_date = models.DateField(verbose_name='Expire le')
     owner = models.ForeignKey(User)
+    quantity = models.IntegerField(default=1, verbose_name=u'Quantité')
+    is_opened = models.BooleanField(default= False, verbose_name=u'Déjà Ouvert?')
+ 
 
     pub_date = models.DateTimeField('date published')
 
