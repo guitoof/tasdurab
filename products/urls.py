115cbe490431943d3ee9748ad8621899b29bbe3f
boisgard <anastasia.boisgard@gmail.com>
2015-02-25 11:49:11 +0100

    working on product_detail view add related products

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from products import views
 
 urlpatterns = patterns('',
     url(r'^$', views.ProductListView.as_view(), name='index'),
+    url(r'^(?P<pk>\d+)/$', views.ProductDetailView.as_view(), name='detail'),
     url(r'create/$', views.ProductCreateView.as_view(), name='create'),
 )
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from products.models import Product, Category
 from products.forms import ProductForm
@@ -12,7 +13,6 @@ class ProductListView(ListView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(ProductListView, self).get_context_data(**kwargs)
-        context['products'] = list(self.get_queryset())
         # Add in a QuerySet of all the categories
         context['category_list'] = Category.objects.all()
         # Add the create Product form to the QuerySet to be display in a modal
@@ -20,8 +20,12 @@ class ProductListView(ListView):
 
         return context
 
+class ProductDetailView(DetailView):
+
+    model = Product
 
 class ProductCreateView(CreateView):
 
     model = Product
     fields = '__all__'
+
